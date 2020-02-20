6cd5dd77348234ee4722768794f4062c54781f00
Chinami Iihama <chinami0420unno@yahoo.co.jp>
2020-02-19 17:35:47 -0800

    finised 5days

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ function displayWeatherInfo(city) {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log("Response: ", response);
-    // debugger;
     var cityName = $("#current-city").text(response.city.name);
     var icon = $("#icon").attr(
       "src",
@@ -34,15 +32,8 @@ function displayWeatherInfo(city) {
     );
     var cityUvindex = $("#uv").text("UV Index: " + response.list[0].main.uv);
     response.list.forEach((el, index) => {
-      if (index === 0 || index % 8 !== 0) return;
-      // var icon = $("#icon").attr(
-      //   "src",
-      //   "http://openweathermap.org/img/wn/" + el.weather[0].icon + "@2x.png"
-      // );
-      // var cityTemp = $("#temp").text("Temprature: " + el.main.temp);
-      // var cityHumidity = $("#humidity").text("Humidity: " + el.main.humidity);
-      // var cityWindSpeed = $("#windspeed").text("Wind Speed: " + el.wind.speed);
-      // var cityUvindex = $("#uv").text("UV Index: " + el.main.uv);
+      if (index % 8 !== 0) return;
+
       var card = $("<div>");
       card.addClass("card");
       card.css("width", "18rem");
@@ -55,26 +46,12 @@ function displayWeatherInfo(city) {
       <li class="list-group-item">${new Date(
         el.dt * 1000
       ).toLocaleString()}</li>
-      <li class="list-group-item">temp ${el.main.temp}</li>
-      <li class="list-group-item">humidity ${el.main.humidity}</li>
+      <li class="list-group-item">Temp:  ${el.main.temp}</li>
+      <li class="list-group-item">Humidity:  ${el.main.humidity}</li>
     </ul>`);
       $(".container-days").append(card);
-
-      // function displayWeatherInfo(lan, lon, cnt) {
-      //   var queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?appid=77db0bed18b09bc55d1f42a05a90a275&lat=" +lat + "&lon=" + lon" + "&cnt=" + cnt
-      // $.ajax({
-      //   // url: new url for uv
-      //   url: queryURL,
-      //   method: "GET"
-      // }).then(function(response) {
-      //   console.log(response);
-      //   response.list.forEach(el => {
-      //     var icon = $("#icon").append(main.icon);
-      //     var cityTemp = $("#temp").text("Temprature: " + el.main.temp);
-      //     var cityHumidity = $("#humidity").text("Humidity: " + el.main.humidity);
-      //     var cityWindSpeed = $("#windspeed").text("Wind Speed: " + el.wind.speed);
-      //     var cityUvindex = $("#uv").text("UV Index: " + el.main.uv);
     });
+
     $.ajax({
       url:
         "https://api.openweathermap.org/data/2.5/uvi?" +
@@ -89,19 +66,6 @@ function displayWeatherInfo(city) {
       $("#uv").text("UV Index: " + response.value);
     });
   });
-
-  // make dynamic columns
-
-  // var upcomingEvents = $("<h2>").text(
-  //   response.upcoming_event_count + " upcoming events"
-  // );
-  // var goToArtist = $("<a>")
-  //   .attr("href", response.url)
-  //   .text("See Tour Dates");
-
-  // Empty the contents of the artist-div, append the new artist content
-  // $("#city-div").empty();
-  // $("#city-div").append(cityName, cityTemp, cityHumidity, cityWindspeed);
 }
 
 $("#submit").on("click", function(e) {
